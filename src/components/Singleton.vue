91ff2a2cd25731738eaeca4d9a36969ee90cc7b7
Nycticorax <mrnycticorax@gmail.com>
2019-10-24 16:13:12 +0200

    Update Singleton.vue

--- src/components/Singleton.vue
+++ src/components/Singleton.vue
@@ -807,7 +807,7 @@ export default {
       firebase.auth().signInWithPopup(provider).then(result => {
           // This gives you a Google Access Token. You can use it to access the Google API.
           // let token = result.credential.accessToken
-          // vm.api.client.setToken({access_token:token}) 
+          // vm.api.client.setToken({access_token:token.toString()}) 
           this.updateUI(true);
         })
         .catch(function(error) {
